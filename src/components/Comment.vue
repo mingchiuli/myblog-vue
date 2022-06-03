6f0cd9cb63720a212c5473858d7828268f1f9ab7
mingchiuli <mingchiuli@163.com>
2022-06-03 19:30:36 +0800

    调整disqus

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -3,13 +3,10 @@
     <div style="margin-left: 25%; max-width: 50%; margin-top: 15px">
       <el-tabs :stretch=true v-model="activeName">
 
-        <el-tab-pane label="Disqus(Please take scientific networking)" name="first">
-          <div class="comments">
-            <Disqus shortname="81-68-192-120"></Disqus>
-          </div>
+        <el-tab-pane label="Disqus" name="first">
+          <div id="disqus" style="width: inherit"></div>
         </el-tab-pane>
-
-        <el-tab-pane label="Utterance(Immune GFW)" name="second">
+        <el-tab-pane label="Utterance" name="second">
           <div id="utterance" style="width: inherit"></div>
         </el-tab-pane>
       </el-tabs>
@@ -21,11 +18,11 @@
 </template>
 <script>
 
-import { Disqus } from 'vue-disqus'
+import DisqusJS from 'disqusjs/dist/disqusjs.es2015.es.js';
+import 'disqusjs/dist/styles/disqusjs.css';
 
 export default {
   name: 'Comment',
-  components:{Disqus},
   data() {
     return {
       activeName: 'first'
@@ -44,6 +41,20 @@ export default {
       utterances.crossorigin = 'anonymous';
       utterances.src = 'https://utteranc.es/client.js';
 
+      const disqus = new DisqusJS({
+        shortname: '81-68-192-120',
+        siteName: 'mingchiuli',
+        identifier: document.location.origin + document.location.pathname + document.location.search,
+        url: document.location.origin + document.location.pathname + document.location.search,
+        title: document.title,
+        api: 'https://disqus.skk.moe/disqus/',
+        apikey: 'dBfYxTiKx3J9iXzyiS000OB5a6Q2nk1dpCtXQZW3UnOKt0AoIvlqZ96bV2FsJiGp',
+        admin: 'mingchiuli',
+        adminLabel: 'Try hard is useless sometimes'
+      });
+      // 是要插入评论的地方
+      disqus.render(document.getElementById('disqus'));
+      // document.getElementById('disqus').appendChild(disqus)
       document.getElementById('utterance').appendChild(utterances);
 
     },
